de41c9c56a13d625d7a37d9fa5fa6e16fb894dc3
logeshwari1 <144526982+logeshwari1@users.noreply.github.com>
2023-09-21 22:17:57 +0530

    Add files via upload

--- main (3).py
+++ main (3).py
@@ -1,10 +1,19 @@
-def isLeapYear(year):
-  if (year % 4 ==0 and year % 100!=0) or year % 400 == 0:
-    return True
-  else:
-    return False
-year=int(input("Enter a year:"))
-if isLeapYear(year):
-  print('is a leap year.'.format(year))
-else:
-  print('{} is not a leap year.'.format(year))
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
+
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Creating objects of the Batsman and Bowler classes and calling the play() method
+if __name__ == "__main__":
+    batsman = Batsman()
+    bowler = Bowler()
+
+    batsman.play()
+    bowler.play()
